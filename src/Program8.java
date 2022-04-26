c6fdb2366c2694545a4d81873c7b8261db1e24a2
jeremy aubrey <jeremyaubrey@gmail.com>
2022-04-26 11:43:51 -0500

    initial commit

---
+++ src/Program8.java
@@ -0,0 +1,48 @@
+//********************************************************************
+//
+//  Author:        Jeremy Aubrey
+//
+//  Program #:     8
+//
+//  File Name:     Program8.java
+//
+//  Course:        COSC-4302 Operating Systems
+//
+//  Due Date:      05/12/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Chapter:       9
+//
+//  Description:   TODO
+//
+//*********************************************************************
+
+public class Program8 {
+
+	public static void main(String[] args) {
+		
+		Program8 test = new Program8();
+		test.developerInfo();
+	}
+	
+	//***************************************************************
+	//
+	//  Method:       developerInfo (Non Static)
+	// 
+	//  Description:  The developer information method of the program.
+	//
+	//  Parameters:   None
+	//
+	//  Returns:      N/A 
+	//
+	//**************************************************************
+	public void developerInfo()
+	{
+	   System.out.println("Name:    Jeremy Aubrey");
+	   System.out.println("Course:  COSC 4302 Operating Systems");
+	   System.out.println("Program: 8");
+
+	}// end developerInfo method
+	
+}// end Program8 class
